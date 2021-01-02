7db96da311c59fb3b25da352f3320fd5682d340a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-01-02 12:25:43 +0100

    Update heap.js

--- _includes/heap.js
+++ _includes/heap.js
@@ -1,2 +1,63 @@
-window.heap = window.heap || [], heap.load = function (e, t) { window.heap.appid = e, window.heap.config = t = t || {}; var r = document.createElement("script"); r.type = "text/javascript", r.async = !0, r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js"; var a = document.getElementsByTagName("script")[0]; a.parentNode.insertBefore(r, a); for (var n = function (e) { return function () { heap.push([e].concat(Array.prototype.slice.call(arguments, 0))) } }, p = ["addEventProperties", "addUserProperties", "clearEventProperties", "identify", "resetIdentity", "removeEventProperty", "setEventProperties", "track", "unsetEventProperty"], o = 0; o < p.length; o++)heap[p[o]] = n(p[o]) };
-heap.load("3575444759");
+(window.lct = window.lct || {}),
+  (lct = {
+    domain: null,
+    fingerprint: null,
+    init: (domain) => {
+      lct.domain = domain;
+      lct.visit();
+      window.addEventListener("popstate", () => {
+        lct.visit();
+      })
+    },
+    collect: () => {
+      const data = new URLSearchParams({
+        host: location.host,
+        hostname: location.hostname,
+        port: location.port ? location.port : 80,
+        path: location.pathname,
+        href: location.href,
+        os: lct.identifyOS(),
+        language: navigator.language || navigator.userLanguage,
+        useragent: navigator.userAgent,
+        domain: lct.domain
+      })
+
+      return data.toString();
+    },
+    visit: async () => {
+      const response = await fetch("https://quest.monitor-api.com/visit", {
+        method: "POST",
+        credentials: "omit",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: lct.collect()
+      })
+      return response.text()
+    },
+    identifyOS: () => {
+      const userAgent = navigator.userAgent,
+        platform = navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+
+      let os = null;
+
+      if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'macos';
+      } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'ios';
+      } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'windows';
+      } else if (/Android/.test(userAgent)) {
+        os = 'android';
+      } else if (!os && /Linux/.test(platform)) {
+        os = 'linux';
+      }
+
+      return os;
+    }
+  });
+
+lct.init("1"); 
